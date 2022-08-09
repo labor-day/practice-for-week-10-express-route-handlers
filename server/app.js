6a002637e4fc4b537e7537534094e3fa2abe36a3
labor-day <shaffer.scott@yahoo.com>
2022-08-09 17:04:00 -0600

    complete

--- server/app.js
+++ server/app.js
@@ -25,6 +25,46 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Body', req.body);
+  next();
+});
+
+app.get("/artists", (req, res) => {
+  let resBody = JSON.stringify(getAllArtists());
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.write(resBody);
+  res.end();
+  return;
+});
+
+app.get("/artists/latest", (req, res) => {
+  let resBody = JSON.stringify(getLatestArtist());
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  return res.end(resBody);
+});
+
+app.get("/artists/latest/albums", (req, res) => {
+  let resBody = JSON.stringify(getAlbumsForLatestArtist());
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  return res.end(resBody);
+});
+
+app.post('/artists', (req, res) => {
+  let resBody = JSON.stringify(addArtist(req.body));
+  res.statusCode = 201;
+  res.setHeader("Content-Type", "application/json");
+  res.write(resBody);
+  res.end();
+  return;
+});
+
+
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
